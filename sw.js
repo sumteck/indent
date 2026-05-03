ede288719c32b4bce0af072743b405a0ccbe8868
sumteck <tksumesh85@gmail.com>
2026-05-03 23:31:37 +0530

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-// Service Worker
-self.addEventListener('install', (event) => {
-  self.skipWaiting();
-});
-
-self.addEventListener('fetch', (event) => {
-  // ബ്രൗസർ നോട്ടിഫിക്കേഷൻ വരാൻ ഈ ഫെച്ച് ഇവന്റ് അത്യാവശ്യമാണ്
-  event.respondWith(fetch(event.request));
-});
